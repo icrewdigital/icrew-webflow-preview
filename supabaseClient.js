84f7db08172c2825b37fc40da40370a14f79658c
Trent Sprules <143155760+icrewdigital@users.noreply.github.com>
2025-06-30 11:39:35 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseKey = 'YOUR_ANON_KEY';  // Use anon public key from API settings
+const supabaseUrl = 'https://lkqfeoyoultfrxkdjfti.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6...'; // Full anon key from API settings
 export const supabase = createClient(supabaseUrl, supabaseKey);
