2a73ec82d9c8a3ccc2613cd74bd257436c58d2f7
Trent Sprules <143155760+icrewdigital@users.noreply.github.com>
2025-06-30 11:57:36 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://lkqfeoyoultfrxkdjfti.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6...'; // Full anon key from API settings
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZ2Zlb3lvdWx0ZnJ4a2RqZnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMDI1NjQsImV4cCI6MjA2NjY3ODU2NH0.pim0hjszO5mlz4OxvXtpvsIHnPugoMEsZhChQaUbMzw'; // From the API Keys section
 export const supabase = createClient(supabaseUrl, supabaseKey);
